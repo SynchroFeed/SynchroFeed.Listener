1d2df62980c848a5a1633992564e9dde50e5d788
Andrew Rissing <arissing@alkamitech.com>
2021-04-07 13:38:45 -0500

    Updated packages to Synchrofeed.Core v1.2.0. Updated project to .NET 5.

---
+++ src/AsyncTaskHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchroFeed.Listener
+{
+    internal static class AsyncTaskHelper
+    {
+        private static readonly TaskFactory MyTaskFactory = new
+            TaskFactory(CancellationToken.None,
+                        TaskCreationOptions.None,
+                        TaskContinuationOptions.None,
+                        TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return MyTaskFactory
+                .StartNew(func)
+                .Unwrap()
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+}
